2ac301530a549a58764e38102a5c582e3025e22c
Andrew Aadland <aa23@protonmail.com>
2021-01-14 16:53:34 -0800

    fix docker registry path

--- docker-build-push.sh
+++ docker-build-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
 TAG="${1}"
-docker build -t ghcr.io/chicken231/helmizer/helmizer:${TAG} -t ghcr.io/chicken231/helmizer/helmizer:latest .
-docker push ghcr.io/chicken231/helmizer/helmizer:${TAG}
+docker build -t docker.pkg.github.com/chicken231/helmizer/helmizer:${TAG} -t docker.pkg.github.com/chicken231/helmizer/helmizer:latest .
+docker push docker.pkg.github.com/chicken231/helmizer/helmizer:${TAG}
